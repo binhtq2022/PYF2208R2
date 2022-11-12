a092f3385ea55822e2e4e5dc12e55ff81c745c34
binhtq2022 <tranbinh@hotmail.com.vn>
2022-11-12 15:45:39 +0700

    QUIZ 1 OF 47 [Bài tập] Chương trình in thông tin cá nhân

---
+++ [Bài tập] Chương trình in thông tin cá nhân/displayInformation.py
@@ -0,0 +1,8 @@
+print("Full name: Tran Quoc Binh")
+print("Date of birth: 02-Sep-1994")
+print("Address: Sai Gon Avenue Appartment, No. 11 street, Tam Binh ward, Thu Duc city, Ho Chi Minh")
+print("Purpose of learning this course is help me get more knowledge to apply to my work")
+print("*"*15)
+
+
+
